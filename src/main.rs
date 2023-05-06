1bcc375c49dfd7fbd372a24edc574ce3910ef10b
Morten Olsen Lysgaard <morten@lysgaard.no>
2023-05-06 20:48:09 +0200

    use itoa dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = itoa::Buffer::new();
+    let printed = buffer.format(128u64);
+    assert_eq!(printed, "128");
+    println!("equal");
 }
